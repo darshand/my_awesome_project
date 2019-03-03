bb98fe8767188ede12e4411f184c2d353fce01b7
Dharshan Bharathuru <dharshandj@gmail.com>
2019-03-03 11:46:47 +0530

    added sin,cos,tan

---
+++ math.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+// This is a sin function
+void sin()
+{
+ 	printf("we are calling sin function");
+}
+
+void cos()
+{
+ 	printf("we are calling cos function");
+}
+
+void tan()
+{
+ 	printf("we are calling tan function");
+}
+
+
+
+
+
+void main()
+{
+sin();
+cos();
+tan();
+}
